67beb3e6edb6b1c7bceb40669e086203aa07e0ea
Wasef <wasef@MC-G-LP-C120-13.local>
2021-10-15 14:29:22 +0200

    add inscription and crate user

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Home from "./page/Home";
 import {Switch,Route,Redirect} from 'react-router-dom';
 import Vrealite from './page/Vrealite';
 import Shop from './page/Shop';
+import Inscription from './page/Inscription';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Route exact path="/" component={Home}  />
       <Route exact path="/Vrealite" component={Vrealite}  />
       <Route exact path="/Shop" component={Shop}  />
+      <Route exact path="/Inscription" component={Inscription}  />
       <Redirect to="/" />
     </Switch>
   );
--- server/src/index.js
+++ server/src/index.js
@@ -11,10 +11,21 @@ const {Users}=require('./models/index')
 const {UsersAPI}=require('./routes/index')
 db.sync()
 
-
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+server.get('/page.html', function(request, response) {
+    Users.create({
+        nom:"emric",
+        prenom : "emric",
+        login : "emric",
+        password : "password",
+        garde:1
+    
+    })
+  });
+
 
 
 app.use('/api/users', UsersAPI)
