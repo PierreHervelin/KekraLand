1d9a4f2168c6e9723cf62a4445433ad0ed69c9d8
Pierre <payangar@MC-G-LP-C120-15.local>
2021-10-18 17:21:55 +0200

    page vreel3

--- client/src/page/Vreel3.js
+++ client/src/page/Vreel3.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
 import { motion } from 'framer-motion';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 const Vreel3 = () => {
     let leftNuage=0,
@@ -42,6 +44,45 @@ const Vreel3 = () => {
     return (
         <main id='vreel3'>
             <div className='background'></div>
+            <div className='panneau-back'></div>
+            <motion.div
+                className='panneau-content'
+                id='cover'
+                animate={{
+                    opacity:[1,1,0,0,1]
+                }}
+                transition={{
+                    duration:20,
+                    times:[0,0.4,0.5,0.9,1],
+                    repeat:Infinity
+                }}
+            ></motion.div>
+            <motion.div
+                className='panneau-content'
+                id='tracklist'
+                animate={{
+                    opacity:[0,0,1,1,0]
+                }}
+                transition={{
+                    times:[0,0.4,0.5,0.9,1],
+                    duration:20,
+                    repeat:Infinity
+                }}
+            ></motion.div>
+            <div className='panneau'></div>
+            <div className='panneau-light'></div>
+            <motion.div 
+                className='panneau-light' 
+                id='panneau-light2'
+                animate={{
+                    opacity:[0,1,1,0,1,0,1,0,1,0]
+                }}
+                transition={{
+                    duration:12,
+                    times:[0,0.01,0.93,0.94,0.95,0.96,0.97,0.98,0.99,1],
+                    repeat:Infinity
+                }}
+            ></motion.div>
             <div className='plateforme'></div>
             <div className='nuages'>
                 <div className='nuage'></div>
