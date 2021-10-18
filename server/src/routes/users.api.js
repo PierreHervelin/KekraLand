15bb96363086862d687aa0afc7ce8b72eb00b7b3
emeric <60581105+Strange2g@users.noreply.github.com>
2021-10-18 17:08:59 +0200

    connexion

--- server/src/routes/users.api.js
+++ server/src/routes/users.api.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const db = require('../../config/database');
 const {Users} = require('../models');
 
-router.get('/', (req, res, next) => {
-  Users.findAll()
+router.get('/', async (req, res, next) => {
+  await Users.findAll()
     .then(users => {
       res.send(users);
       res.sendStatus(200).json(users);
@@ -23,7 +23,7 @@ router.get('/destroy', (req, res) => {
   .catch(err => console.log(err))
 });
 
-router.get('/create', (req, res) => {
+router.post('/create', (req, res) => {
   Users.create()
     .then(users => {
       console.log(users)
@@ -31,5 +31,16 @@ router.get('/create', (req, res) => {
     })
     .catch(err => console.log(err))
 })
+
+router.post('/login', async (req, res) => {
+  const {login, password } = req.body
+
+  const user = await Users.findOne({where: { login: login } });
+
+  if(!user) res.json({ error: "L'utilisateur n'existe pas"});
+  if(user.password !== password) res.json({ error: "Erreur de mot de passe"});
+
+  res.json({ error: "Vous etes bien connecté"});
+});
 
 module.exports = router 
