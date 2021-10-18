840cdaa8a3aa7ab4329522fc91e718afea727231
emeric <60581105+Strange2g@users.noreply.github.com>
2021-10-18 17:19:58 +0200

    connexion

---
+++ client/src/page/Connexion.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import axios from "axios";
+import '../styles/index.css';
+
+const Connexion = () => {
+
+    const [login, setLogin] = useState("");
+    const [password, setPassword] = useState("");
+
+    const Login =() => {
+        const data = {login: login, password: password};
+        axios.post("http://localhost:3001/api/users/login", data).then((response) => {
+            console.log(response.data);
+        });
+    };
+
+    return (
+        <div className= "container">
+            <div className = "form">
+                <h1>Se connecter</h1>
+                <input type = "text" onChange={(event) => {
+                    setLogin(event.target.value);
+                }}  placeholder = "Login" required/>
+                <br/>
+                <input type = "password" onChange={(event) => {
+                    setPassword(event.target.value);
+                }} placeholder = "Password" required/>
+                <button onClick={Login}>Se connecter</button>
+            </div>
+        </div>
+    );
+};
+
+export default Connexion;
