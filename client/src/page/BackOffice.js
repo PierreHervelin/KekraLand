708ef7d00f687059f23b75f08dcd21d7664073e9
Pierre <payangar@MC-G-LP-C120-15.local>
2021-11-08 18:37:07 +0100

    backoffice 1

---
+++ client/src/page/BackOffice.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const BackOffice = () => {
+    return (
+        <div className='form'>
+            <div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default BackOffice;
