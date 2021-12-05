1d8b3326f623fe5c2738dc625f67fb46eb817841
Pierre <hervelinp.code@gmail.com>
2021-12-05 13:31:45 +0100

    style news

--- client/src/components/News.js
+++ client/src/components/News.js
@@ -1,8 +1,31 @@
-import React, { useEffect, useRef } from 'react';
-import ConcertImage from '../asset/img/kekraconcert2.jpg'
+import React, { useEffect, useRef, useState } from 'react';
+import clip from '../asset/video/kekra_video2.mp4'
+import concert from '../asset/video/kekraconcert.mp4'
 
 const News = () => {
     const ref=useRef({})
+
+    useEffect(()=>{
+        const interval=setInterval(() => {
+            if(ref.current.box.classList.contains('move')){
+                ref.current.box.classList.remove('move')
+            }else{
+                ref.current.box.classList.add('move')
+            }
+        }, 8000);
+        const interval2=setInterval(() => {
+            if(ref.current.concert.classList.contains('active')){
+                ref.current.concert.classList.remove('active')
+            }else{
+                ref.current.concert.classList.add('active')
+            }
+        }, 10000);
+
+        return ()=>{
+            clearInterval(interval)
+            clearInterval(interval2)
+        }
+    },[])
 
     return (
         <div className='news'>
@@ -13,6 +36,14 @@ const News = () => {
                 <div className='concert observe'>
                     <h2>KEKRA EN CONCERT</h2>
                     <div className='img'>
+                        <video 
+                            autoPlay
+                            muted
+                            loop
+                            ref={el=>ref.current.concert=el}
+                        >
+                            <source src={concert}/>
+                        </video>
                         <div className='back'></div>
                         <div className='infos'>
                             <h3>KEKRA</h3>
@@ -26,14 +57,35 @@ const News = () => {
 
                 <div className='actu' ref={el=>ref.current['actu']=el}>
                     <div className='top observe'>
-                        <div className='left'>
+                        <div 
+                            className='left'
+                            ref={el=>ref.current.box=el}
+                        >
+                            <div className='box-container'>
+                                <div className='box' id='box1'></div>
+                                <div className='box' id='box2'></div>
+                            </div>
                             <div className='infos'>
                                 <h2>box kekra</h2>
                                 <button>voir plus</button>
                             </div>
                         </div>
                         <div className='right'>
-                            <div></div>
+                            <div>
+                                <video autoPlay muted loop>
+                                    <source src={clip}/>
+                                </video>
+                                <div 
+                                    className='back'
+                                    onClick={
+                                        ()=>{
+                                            ref.current.clip.click()
+                                        }
+                                    }
+                                >
+                                    <p>voir le clip</p>
+                                </div>
+                            </div>
                             <div></div>
                         </div>
                     </div>
@@ -49,6 +101,11 @@ const News = () => {
                     </div>
                 </div>
             </div>
+            <a 
+                href='https://www.youtube.com/watch?v=G9BwHA13jJc'
+                target="_blank"
+                ref={el=>ref.current.clip=el}
+            />
         </div>
     );
 };
